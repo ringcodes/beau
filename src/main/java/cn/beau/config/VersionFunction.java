addfd1cb29140eb053d4c4e303912a96fd60456c
liushilin <liushilin@codemao.cn>
2022-01-08 00:16:26 +0800

    静态文件添加版本号

--- src/main/java/cn/beau/config/VersionFunction.java
+++ src/main/java/cn/beau/config/VersionFunction.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cn.beau.config;
 
 import org.beetl.core.Context;
@@ -11,6 +27,9 @@ import java.util.Map;
 import java.util.StringJoiner;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * 版本号
+ */
 public class VersionFunction implements Function {
     private Map<String, String> versionMap = new ConcurrentHashMap<>();
     private final static String DELIMITER = "?";
